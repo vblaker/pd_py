4a4a76f3d3b8273657124f2bbcd61a75618e50eb
Vadim Blaker <vblaker@gmail.com>
2017-09-19 13:39:04 -0500

    PD PY Project

---
+++ data_cap.py
@@ -0,0 +1,9 @@
+import capture_usbpd
+import detect
+
+ports, unique_ids = detect.detect_pd()
+
+#num = int(input('Number of samples to acquire: ')) * 6
+
+for i in range(len(ports)):
+    capture_usbpd.capture_usbpd(port=ports[i], mode='iv', num=5)
