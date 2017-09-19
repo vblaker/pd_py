79101dfae150acc463c5a7feb6a6b1030c22587b
Vadim Blaker <vblaker@gmail.com>
2017-09-19 13:15:39 -0500

    PD PY Project

--- capture_usbpd.py
+++ capture_usbpd.py
@@ -27,8 +27,9 @@
 #==========================================================================
 # IMPORTS
 #==========================================================================
-from pd_py import *
-
+#from pd_py import *
+import pd_py
+import sys
 
 #==========================================================================
 # PRINT FUNCTIONS
@@ -39,34 +40,34 @@ def print_pd_info (idx, info, info_type):
     if info_type != 'PD':
         return True
 
-    cc = (info.events & PD_EVENT_USBPD_CC_MASK) >> PD_EVENT_USBPD_CC_SHIFT
+    cc = (info.events & pd_py.PD_EVENT_USBPD_CC_MASK) >> pd_py.PD_EVENT_USBPD_CC_SHIFT
     print '%d,' % (cc + 1),
 
-    if info.events & PD_EVENT_USBPD_POL_CHANGE:
+    if info.events & pd_py.PD_EVENT_USBPD_POL_CHANGE:
         print 'POL_CHANGE,'
         return False
-    elif (info.events & PD_EVENT_USBPD_SOP_MASK) == PD_EVENT_USBPD_HARD_RESET:
+    elif (info.events & pd_py.PD_EVENT_USBPD_SOP_MASK) == pd_py.PD_EVENT_USBPD_HARD_RESET:
         print 'HARD_RESET,'
         return False
-    elif (info.events & PD_EVENT_USBPD_SOP_MASK) == PD_EVENT_USBPD_CABLE_RESET:
+    elif (info.events & pd_py.PD_EVENT_USBPD_SOP_MASK) == pd_py.PD_EVENT_USBPD_CABLE_RESET:
         print 'CABLE_RESET,'
         return False
 
-    pos_err = info.status & PD_STATUS_USBPD_ERR_MASK
-    if pos_err == PD_STATUS_USBPD_ERR_PREAMBLE:
+    pos_err = info.status & pd_py.PD_STATUS_USBPD_ERR_MASK
+    if pos_err == pd_py.PD_STATUS_USBPD_ERR_PREAMBLE:
         print 'ERR_PREAMBLE,',
-    elif pos_err == PD_STATUS_USBPD_ERR_SOP:
+    elif pos_err == pd_py.PD_STATUS_USBPD_ERR_SOP:
         print 'ERR_SOP,',
-    elif pos_err == PD_STATUS_USBPD_ERR_HEADER:
+    elif pos_err == pd_py.PD_STATUS_USBPD_ERR_HEADER:
         print 'ERR_HEADER,',
-    elif pos_err == PD_STATUS_USBPD_ERR_DATA:
+    elif pos_err == pd_py.PD_STATUS_USBPD_ERR_DATA:
         print 'ERR_DATA,',
-    elif pos_err == PD_STATUS_USBPD_ERR_CRC:
+    elif pos_err == pd_py.PD_STATUS_USBPD_ERR_CRC:
         print 'ERR_CRC,',
-    elif pos_err == PD_STATUS_USBPD_ERR_EOP:
+    elif pos_err == pd_py.PD_STATUS_USBPD_ERR_EOP:
         print 'ERR_EOP,',
 
-    if info.status & PD_STATUS_USBPD_ERR_BAD_CRC:
+    if info.status & pd_py.PD_STATUS_USBPD_ERR_BAD_CRC:
         print 'BAD_CRC,',
     elif pos_err == 0:
         print 'OK,',
@@ -105,28 +106,28 @@ def print_pd_packets (idx, info, preamble, header, crc, data):
     if not print_pd_info(idx, info, 'PD'):
         return
 
-    pos_err = info.status & PD_STATUS_USBPD_ERR_MASK
+    pos_err = info.status & pd_py.PD_STATUS_USBPD_ERR_MASK
 
-    if (pos_err == PD_STATUS_USBPD_ERR_PREAMBLE or
-        pos_err == PD_STATUS_USBPD_ERR_SOP):
+    if (pos_err == pd_py.PD_STATUS_USBPD_ERR_PREAMBLE or
+        pos_err == pd_py.PD_STATUS_USBPD_ERR_SOP):
         print ''
         return
 
     sop_str = [ 'SOP', 'SOP\'', 'SOP"', 'SOP\'D', 'SOP"D', '', '', '' ]
-    sop = info.events & PD_EVENT_USBPD_SOP_MASK
+    sop = info.events & pd_py.PD_EVENT_USBPD_SOP_MASK
 
     print '%s,' % sop_str[sop],
 
-    if pos_err == PD_STATUS_USBPD_ERR_HEADER:
+    if pos_err == pd_py.PD_STATUS_USBPD_ERR_HEADER:
         print ''
         return
 
-    if info.events & PD_EVENT_USBPD_EXTENDED_HEADER:
+    if info.events & pd_py.PD_EVENT_USBPD_EXTENDED_HEADER:
         print '%04x;%04x,' \
-            % (header & PD_HEADER_USBPD_STANDARD_MASK,
-               (header & PD_HEADER_USBPD_EXTENDED_MASK) >> 16),
+            % (header & pd_py.PD_HEADER_USBPD_STANDARD_MASK,
+               (header & pd_py.PD_HEADER_USBPD_EXTENDED_MASK) >> 16),
     else:
-        print '%04x,' % (header & PD_HEADER_USBPD_STANDARD_MASK),
+        print '%04x,' % (header & pd_py.PD_HEADER_USBPD_STANDARD_MASK),
 
     if len(data) > 0:
         for offset in range(len(data)):
@@ -134,12 +135,12 @@ def print_pd_packets (idx, info, preamble, header, crc, data):
 
     print ",",
 
-    if (pos_err == PD_STATUS_USBPD_ERR_DATA or
-        pos_err == PD_STATUS_USBPD_ERR_CRC):
+    if (pos_err == pd_py.PD_STATUS_USBPD_ERR_DATA or
+        pos_err == pd_py.PD_STATUS_USBPD_ERR_CRC):
         print ''
         return
 
-    if info.status & PD_STATUS_USBPD_ERR_BAD_CRC:
+    if info.status & pd_py.PD_STATUS_USBPD_ERR_BAD_CRC:
         print '%08x(NOT OK)' % crc
     else:
         print '%08x(OK)' % crc
@@ -148,8 +149,8 @@ def print_pd_iv (idx, info, val):
     if not print_pd_info(idx, info, 'PD_IV'):
         return
 
-    iv_type = ((info.events & PD_EVENT_USBPD_IV_SOURCE_MASK)
-               >> PD_EVENT_USBPD_IV_SHIFT)
+    iv_type = ((info.events & pd_py.PD_EVENT_USBPD_IV_SOURCE_MASK)
+               >> pd_py.PD_EVENT_USBPD_IV_SHIFT)
     iv_type_str = [
         'Vbus Voltage', 'Vbus Current', 'Vconn Voltage', 'Vconn Current',
         'CC1 Voltage', 'CC1 Current', 'CC2 Voltage', 'CC2 Current',
@@ -162,66 +163,66 @@ def print_pd_iv (idx, info, val):
 #==========================================================================
 # DUMP FUNCTIONS
 #==========================================================================
-def dump_pd_raw (num_packets):
+def dump_pd_raw (pd, num_packets):
     packetnum = 0
 
     print "index,time(us),PD,status,cc,preamble,bits"
 
     while (num_packets <= 0 or packetnum < num_packets):
         # maximum bit length of USB PD v3.0 is 2769 bits = 347 bytes
-        ret, info, bl, pl, bits = pd_usbpd_read_bits(pd, 512)
-        if ret == PD_READ_EMPTY:
+        ret, info, bl, pl, bits = pd_py.pd_usbpd_read_bits(pd, 512)
+        if ret == pd_py.PD_READ_EMPTY:
             continue
 
         if ret < 0:
             print ('Failed to capture PD on port: %d(%s)' %
-                   (ret, pd_status_string(ret)))
+                   (ret, pd_py.pd_status_string(ret)))
             break
 
         print_bits(packetnum, info, bits, pl, bl)
 
         packetnum += 1
 
-def dump_pd_data (num_packets):
+def dump_pd_data (pd, num_packets):
     packetnum = 0
 
     print "index,time(us),PD,cc,status,sop,header,data0 ... dataN,crc"
 
     while (num_packets <= 0 or packetnum < num_packets):
         # maximum bit length of USB PD v3.0 is 2769 bits = 347 bytes
-        ret, info, pl, header, crc, data = pd_usbpd_read_data(pd, 512)
-        if ret == PD_READ_EMPTY:
+        ret, info, pl, header, crc, data = pd_py.pd_usbpd_read_data(pd, 512)
+        if ret == pd_py.PD_READ_EMPTY:
             continue
 
         if ret < 0:
             print ('Failed to capture PD on port: %d(%s)' %
-                   (ret, pd_status_string(ret)))
+                   (ret, pd_py.pd_status_string(ret)))
             break
 
         print_pd_packets(packetnum, info, pl, header, crc, data)
 
         packetnum += 1
 
-def dump_pd_iv (num_packets):
+def dump_pd_iv (pd, num_packets):
     packetnum = 0
 
     print "index,time(us),PD_IV,type,value"
 
     while (num_packets <= 0 or packetnum < num_packets):
-        ret, info, val = pd_usbpd_read_iv(pd)
-        if ret == PD_READ_EMPTY:
+        ret, info, val = pd_py.pd_usbpd_read_iv(pd)
+        if ret == pd_py.PD_READ_EMPTY:
             continue
 
         if ret < 0:
             print ('Failed to capture PD on port: %d(%s)' %
-                   (ret, pd_status_string(ret)))
+                   (ret, pd_py.pd_status_string(ret)))
             break
 
         print_pd_iv(packetnum, info, val)
 
         packetnum += 1
 
-def dump_pd_all (num_packets):
+def dump_pd_all (pd, num_packets):
     packetnum = 0
 
     print "index,time(us),PD,cc,status,sop,header,data0 ... dataN,crc"
@@ -229,22 +230,22 @@ def dump_pd_all (num_packets):
     print "index,time(us),PD_IV,type,value"
 
     while (num_packets <= 0 or packetnum < num_packets):
-        ret, info, pl, header, crc, data = pd_usbpd_read_data(pd, 512)
-        if ret != PD_READ_EMPTY:
+        ret, info, pl, header, crc, data = pd_py.pd_usbpd_read_data(pd, 512)
+        if ret != pd_py.PD_READ_EMPTY:
             if ret < 0:
                 print ('Failed to capture PD on port: %d(%s)' %
-                       (ret, pd_status_string(ret)))
+                       (ret, pd_py.pd_status_string(ret)))
                 break
 
             print_pd_packets(packetnum, info, pl, header, crc, data)
 
             packetnum += 1
 
-        ret, info, val = pd_usbpd_read_iv(pd)
-        if ret != PD_READ_EMPTY:
+        ret, info, val = pd_py.pd_usbpd_read_iv(pd)
+        if ret != pd_py.PD_READ_EMPTY:
             if ret < 0:
                 print ('Failed to capture PD on port: %d(%s)' %
-                       (ret, pd_status_string(ret)))
+                       (ret, pd_py.pd_status_string(ret)))
                 break
 
             print_pd_iv(packetnum, info, val)
@@ -274,38 +275,48 @@ For product documentation and specifications, see www.totalphase.com."""
 #==========================================================================
 # MAIN PROGRAM
 #==========================================================================
-if (len(sys.argv) < 3):
-    print_usage()
-    sys.exit()
+def capture_usbpd(port, mode, num):
 
-port = int(sys.argv[1])
-mode = sys.argv[2]
-num  = int(sys.argv[3]) if len(sys.argv) == 4 else 0
+    '''
+    if (len(sys.argv) < 3):
+        print_usage()
+        sys.exit()
 
-# Open the device
-pd = pd_open(port)
-if (pd <= 0):
-    print ('Unable to open PD Analyzer on port %d: %d(%s)' %
-           (port, pd, pd_status_string(pd)))
-    sys.exit(1)
-print 'Opened PD Analyzer on port %d' % port;
+    port = int(sys.argv[1])
+    mode = sys.argv[2]
+    num  = int(sys.argv[3]) if len(sys.argv) == 4 else 0
+    '''
 
-# Start capture
-pd_capture_start(pd)
+    #port = 0
+    #mode = 'iv'
+    #num  = 10
 
-print 'Start capturing'
-if mode == 'raw':
-    dump_pd_raw(num)
-if mode == 'data':
-    dump_pd_data(num)
-if mode == 'iv':
-    dump_pd_iv(num)
-if mode == 'all':
-    dump_pd_all(num)
+    # Open the device
+    pd = pd_py.pd_open(port)
+    if (pd <= 0):
+        print ('Unable to open PD Analyzer on port %d: %d(%s)' %
+               (port, pd, pd_py.pd_status_string(pd)))
+        sys.exit(1)
+    print 'Opened PD Analyzer on port %d' % port;
 
-# Stop capture
-pd_capture_stop(pd)
+    # Start capture
+    pd_py.pd_capture_start(pd)
 
-# Close the device
-pd_close(pd)
+    print 'Start capturing'
+    if mode == 'raw':
+        dump_pd_raw(num)
+    if mode == 'data':
+        dump_pd_data(num)
+    if mode == 'iv':
+        dump_pd_iv(pd, num)
+    if mode == 'all':
+        dump_pd_all(num)
 
+    # Stop capture
+    pd_py.pd_capture_stop(pd)
+
+    # Close the device
+    pd_py.pd_close(pd)
+
+
+#capture_usbpd(0, 'iv', 10)
