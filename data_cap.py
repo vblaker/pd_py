64e6f4604378e0176a060c4ba342de2c90c1c38f
vblaker <vblaker@gmail.com>
2017-09-19 21:24:48 -0500

    Update

--- data_cap.py
+++ data_cap.py
@@ -1,10 +1,34 @@
 import capture_usbpd
 import detect
 
+# Define headers
+iv_type_str = ['Vbus Voltage', 'Vbus Current', 'Vconn Voltage', 'Vconn Current',
+    'CC1 Voltage', 'CC1 Current', 'CC2 Voltage', 'CC2 Current']
+
+header_list = ['Time (s)', 'VBUS Voltage (V)', 'VBUS Current (A)', 'VCONN Voltage (V)',
+               'VCONN Current (A)', 'CC1 Voltage (V)', 'CC1 Current (A)',
+               'CC2 Voltage (V)', 'CC2 Current (A)']
+
+# Set debug flag
+debug = 1
+
+data_list = [Time_stamp, VBUS_volts, VBUS_curr, VCONN_volts, VCONN_curr, CC1_volts, CC1_curr, CC2_volts, CC2_curr]
+header_dict = {}
+
+# Detect PD Analyzers
 ports, unique_ids = detect.detect_pd()
 
-#num = int(input('Number of samples to acquire: ')) * 6
-
+# Capture PD Data
 for i in range(len(ports)):
     data = capture_usbpd.capture_usbpd(port=ports[i], mode='iv', num=5)
-    print (data)
+
+    if (debug == 1):
+        print (data)
+
+
+# Parse out data
+
+
+# Create Data Dictionary
+data_dictionary = dict(zip(header_list, data_list))
+
