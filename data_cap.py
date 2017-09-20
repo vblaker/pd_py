fa045e3371ece6200fd5af9ebb1a28de19894779
Vadim Blaker <vblaker@gmail.com>
2017-09-20 16:08:36 -0500

    Update

--- data_cap.py
+++ data_cap.py
@@ -13,7 +13,7 @@ header_list = ['Time (s)', 'VBUS Voltage (V)', 'VBUS Current (A)', 'VCONN Voltag
 
 # Set debug and plot flags
 debug = 0
-plot = 0
+plot = 1
 
 # Initialize arrays/lists
 Time_stamp = []
@@ -25,16 +25,17 @@ CC1_volts = []
 CC1_curr = []
 CC2_volts = []
 CC2_curr = []
+
 data_list = [Time_stamp, VBUS_volts, VBUS_curr, VCONN_volts, VCONN_curr, CC1_volts, CC1_curr, CC2_volts, CC2_curr]
 header_dict = {}
-
 
 # Detect PD Analyzers
 ports, unique_ids = detect.detect_pd()
 
-# Capture PD Data
+# Capture PD Data.
+# Sampling is roughly at 8 ms/sample or 125 samples/second. One sample is approximately 8 items on the list
 for i in range(len(ports)):
-    data = capture_usbpd.capture_usbpd(port=ports[i], mode='iv', num=500, debug=debug)
+    data = capture_usbpd.capture_usbpd(port=ports[i], mode='iv', num=625, debug=debug)
 
 if debug == 1:
     print (data)
@@ -42,8 +43,9 @@ if debug == 1:
 
 # Parse out data
 for i in range(len(data)):
-    Time_stamp.append(data[i][0])
-    if data[i][1] == 'Vbus Voltage':
+    if (i % 8) == 0:
+        Time_stamp.append(data[i][0])
+    elif data[i][1] == 'Vbus Voltage':
         VBUS_volts.append(data[i][2])
     elif data[i][1] == 'Vbus Current':
         VBUS_curr.append(data[i][2])
@@ -60,15 +62,12 @@ for i in range(len(data)):
     elif data[i][1] == 'CC2 Current':
         CC2_curr.append(data[i][2])
 
-Time_stamp = Time_stamp[::8]
-
-print('Number of VBUS_voltage samples is {}'.format(len(VBUS_volts)))
-print('Number of Time Samples is {}'.format(len(Time_stamp)))
-
 # Create Data Dictionary
 data_dictionary = dict(zip(header_list, data_list))
 if debug == 1:
     print(data_dictionary)
+    print('Number of VBUS_voltage samples is {}'.format(len(VBUS_volts)))
+    print('Number of Time Samples is {}'.format(len(Time_stamp)))
 
 if plot == 1:
     # Call plot function to display IV data
