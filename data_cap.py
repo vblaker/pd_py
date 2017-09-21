48f636dd4aa6eda57245de5d18f7fb508715c9d2
vblaker <vblaker@gmail.com>
2017-09-21 17:38:51 -0500

    Update

--- data_cap.py
+++ data_cap.py
@@ -2,6 +2,7 @@ import capture_usbpd
 import detect
 import timeit
 import plot_data
+import string
 
 # Define headers
 iv_type_str = ['Vbus Voltage', 'Vbus Current', 'Vconn Voltage', 'Vconn Current',
@@ -13,7 +14,7 @@ header_list = ['Time (s)', 'VBUS Voltage (V)', 'VBUS Current (A)', 'VCONN Voltag
 
 # Set debug and plot flags
 debug = 0
-plot = 1
+plot = 0
 
 # Initialize arrays/lists
 Time_stamp = []
@@ -35,15 +36,15 @@ ports, unique_ids = detect.detect_pd()
 # Capture PD Data.
 # Sampling is roughly at 8 ms/sample or 125 samples/second. One sample is approximately 8 items on the list
 for i in range(len(ports)):
-    data = capture_usbpd.capture_usbpd(port=ports[i], mode='iv', num=625, debug=debug)
+    data = capture_usbpd.capture_usbpd(port=ports[i], mode='iv', num=500, debug=debug)
 
 if debug == 1:
     print (data)
 
 
-# Parse out data
+# Parse out data. For time stamp pick every 8th data point
 for i in range(len(data)):
-    if (i % 8) == 0:
+    if (i % len(iv_type_str)) == 0:
         Time_stamp.append(data[i][0])
     elif data[i][1] == 'Vbus Voltage':
         VBUS_volts.append(data[i][2])
@@ -66,10 +67,45 @@ for i in range(len(data)):
 data_dictionary = dict(zip(header_list, data_list))
 if debug == 1:
     print(data_dictionary)
-    print('Number of VBUS_voltage samples is {}'.format(len(VBUS_volts)))
-    print('Number of Time Samples is {}'.format(len(Time_stamp)))
+
+# Iterate through data_dictionary and 0-pad empty lists
+for key, value in data_dictionary.items():
+    if len(value) == 0:
+        data_dictionary[key] = [0.0] * len(data_dictionary['Time (s)'])
+    print('Number of samples in {} is {}'.format(key, len(data_dictionary[key])))
+
 
 if plot == 1:
     # Call plot function to display IV data
     plot_data.plot_data(data_dictionary)
 
+
+
+
+# Determine a smallest number of data points on all the lists in dictionary for proper file dump
+data_minimum_length = min([len(value) for key, value in data_dictionary.iteritems()])
+
+# Iterate through dictionary to print out data to screen
+j = 0
+for i in range(data_minimum_length):
+    str_list = []
+    #for j in range(len(header_list)):
+    h = 0
+    for key in data_dictionary:
+        header = header_list[h]
+        str_list.append(str(data_dictionary[header][j]))
+        h += 1
+    j += 1
+
+    # Print header only first time
+    if j - 1 == 0: print(",".join(header_list))
+    print(",".join(str_list))
+
+
+'''
+# Write Data to file
+with open('my_file.csv', 'w') as f:
+    f.write(string(header_list))
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in data_dictionary.items()]
+f.close()
+'''
