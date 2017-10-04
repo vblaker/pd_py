c3751fc9a61af1d4530c1e5523e55cbf54810b54
vblaker <vblaker@gmail.com>
2017-10-04 18:35:55 -0500

    Update

--- read_power_supply_info.py
+++ read_power_supply_info.py
@@ -1,8 +1,5 @@
 import csv
 
-#for line in reversed(open("filename").readlines()):
-#    print line.rstrip()
-
 debug = 1
 
 with open('power_supply_info.csv', 'r') as csv_file:
@@ -10,7 +7,12 @@ with open('power_supply_info.csv', 'r') as csv_file:
     for row in readCSV:
         if row:
             if row[0] == 'TimeStamp':
-                header_list = row[:-1]                                                  # Slice OFF last empty value
+                # Check for non-empty values
+                header_list = []
+                for i in range(len(row)):
+                    if row[i]: header_list.append(row[i])
+                #header_list = row[:-1]                                                  # Slice OFF last empty value
+
                 if debug == 1: print(header_list)
                 # Identify all the Column Headers
                 header_dictionary = dict(zip(header_list, range(len(header_list))))     # Create header dictionary
