7141313dcd59f9fd1b2fc53b9ad6a0afd5359b33
vblaker <vblaker@gmail.com>
2017-10-04 18:26:27 -0500

    Update

--- read_power_supply_info.py
+++ read_power_supply_info.py
@@ -10,18 +10,20 @@ with open('power_supply_info.csv', 'r') as csv_file:
     for row in readCSV:
         if row:
             if row[0] == 'TimeStamp':
-                header_list = row
-                print(header_list)
+                header_list = row[:-1]                                                  # Slice OFF last empty value
+                if debug == 1: print(header_list)
                 # Identify all the Column Headers
                 header_dictionary = dict(zip(header_list, range(len(header_list))))     # Create header dictionary
-                data_dictionary = {}
-                data_dictionary = {header: [] for header in header_list}    # Init data dictionary
+                data_dictionary = {key: [] for key in header_dictionary}                # Init data dictionary
                 continue
             try:
+                test = int(row[0][:4])
                 time_stamp = row[0]
-                print(int(time_stamp[:4]))
-                # Append values to data dictionary
-                for header in header_list:
+                if debug == 1:
+                    print(time_stamp)
+                for header in header_dictionary:                                        # Append values to data dictionary
                     data_dictionary[header].append(row[header_dictionary[header]])
             except ValueError:
                     print('Skipping row {0}'.format(row))
+
+print(data_dictionary)
