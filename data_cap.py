2b98ed85bbe74b806c335a85fa920ccdefbee909
Vadim Blaker <vblaker@gmail.com>
2017-10-11 10:54:11 -0500

    Updates

--- data_cap.py
+++ data_cap.py
@@ -1,6 +1,6 @@
 import capture_usbpd
 import detect
-import timeit
+import datetime
 import plot_data
 import save_to_csv
 
@@ -12,9 +12,10 @@ header_list = ['Time', 'Vbus Voltage', 'Vbus Current', 'Vconn Voltage', 'Vconn C
     'CC1 Voltage', 'CC1 Current', 'CC2 Voltage', 'CC2 Current', 'Real-Time Stamp']
 
 # Set column positions for data labels and data values
+col_data_time = 0
 col_data_label = 1
 col_data_val = 2
-
+col_data_real_time = 3
 
 # Set debug and plot flags
 debug = 0
@@ -33,7 +34,6 @@ for i in range(len(ports)):
 if debug == 1:
     print (data)
 
-
 header_dictionary = dict(zip(header_list, range(len(header_list))))     # Create header dictionary
 data_dictionary = {key: [] for key in header_dictionary}                # Init data dictionary
 
@@ -41,16 +41,22 @@ if debug == 1:
     print(data_dictionary)
 
 # Parse out data and assign real-time labels to samples
-#  time_stamp = datetime.datetime.strptime('2017-10-10 12:05:48.442000', '%Y-%m-%d %H:%M:%S.%f') TODO
-#  time_stamp + datetime.timedelta(seconds=120) TODO
+#  time_stamp = datetime.datetime.strptime('2017-10-10 12:05:48.442000', '%Y-%m-%d %H:%M:%S.%f')
+#  time_stamp + datetime.timedelta(seconds=120)
 
+# Extract real-time capture start from the first sample in the data set
+real_time_cap_start = data[0][col_data_real_time] - datetime.timedelta(seconds=data[0][col_data_time])
+
+# Iterate through the data and populate data_dictionary
 for i in range(len(data)):
     for j in range(len(iv_type_str)):
-        if data[i][col_data_label] == iv_type_str[j]: data_dictionary[(iv_type_str[j])].append(data[i][col_data_val])
-        else: data_dictionary[(iv_type_str[j])].append('-')
-
-    data_dictionary['Time'].append(data[i][0])
-    data_dictionary['Real-Time Stamp'].append(data[i][3])
+        if data[i][col_data_label] == iv_type_str[j]:
+            data_dictionary[(iv_type_str[j])].append(data[i][col_data_val])
+        else:
+            data_dictionary[(iv_type_str[j])].append('-')
+    data_dictionary['Time'].append(data[i][col_data_time])
+    data_dictionary['Real-Time Stamp'].\
+        append(real_time_cap_start + datetime.timedelta(seconds=float(data[i][col_data_time])))
 
 # Determine a largest number of data points on all the lists in dictionary for proper file dump
 data_min_length = min([len(value) for key, value in data_dictionary.iteritems()])
